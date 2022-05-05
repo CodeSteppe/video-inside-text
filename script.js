cfb940e23737278ca7671c9a40991d0aa7d17400
bruce_zhang <bruce_zhang@trendmicro.com>
2022-05-05 16:17:29 +0800

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+const pathWidth = 363;
+const pathHeight = 145;
+
+const root = document.documentElement;
+
+root.style.setProperty('--pathWidth', pathWidth + 'px');
+root.style.setProperty('--pathHeight', pathHeight + 'px');
