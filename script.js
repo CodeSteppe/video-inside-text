a207508f2640524dabf573e3d5486757e9b507ca
MudOnTire <895157882@qq.com>
2022-05-05 22:46:33 +0800

    remove js

--- script.js
+++
@@ -1,7 +0,0 @@
-const pathWidth = 363;
-const pathHeight = 145;
-
-const root = document.documentElement;
-
-root.style.setProperty('--pathWidth', pathWidth + 'px');
-root.style.setProperty('--pathHeight', pathHeight + 'px');
